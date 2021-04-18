9f05591d16e92e7f17d04ff5aea71016e9ec6518
nkakavros <n.kakavros@live.com>
2021-04-18 18:37:55 -0400

    BowlingGameTest  - TestGutterGame - Fail - (Expected 0 but was -1)

--- BowlingGame/BowlingGameTest.cs
+++ BowlingGame/BowlingGameTest.cs
@@ -17,6 +17,12 @@ namespace BowlingGame
         public void TestGutterGame()
         {
             Game game = NewGame;
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+            Assert.AreEqual(0, game.Score());
         }
     }
 }
