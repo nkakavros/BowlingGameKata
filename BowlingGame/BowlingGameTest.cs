0b6833b4cfdf72557bc58754f9638bfcc010e3f1
nkakavros <n.kakavros@live.com>
2021-04-18 18:23:06 -0400

    Bowling Game - Initial Check In
    Bowling Game - Initial Check In.

---
+++ BowlingGame/BowlingGameTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+
+namespace BowlingGame
+{
+    [TestFixture]
+    public class BowlingGameTest
+    {
+
+        #region Private Members
+
+        private Game NewGame => new Game();
+
+        #endregion
+
+        [Test]
+        public void TestGutterGame()
+        {
+            Game game = NewGame;
+        }
+    }
+}
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BowlingGame
+{
+    public class Game
+    {
+
+        #region Private Members
+
+        private int[] knockedDownPins = new int[21];
+        private int rollCounter = 0;
+
+        #endregion
+
+        public void Roll(int pins)
+        {
+            if (pins == 10)
+            {
+                rollCounter++;
+            }
+            knockedDownPins[rollCounter++] = pins;
+        }
+
+        public int Score()
+        {
+            return -1;
+        }
+    }
+}
