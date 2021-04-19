7c2704f541ac17d1860288ee28d7c50b99dae58d
nkakavros <n.kakavros@live.com>
2021-04-18 21:11:12 -0400

    BowlingGame: Step 41 - Third Test Fails - Expected 16 but was 13 (Update to Score Method of Game Class).

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -27,10 +27,12 @@ namespace BowlingGame
         public int Score()
         {
             int score = 0;
-            for (int i = 0; i < rolls.Length; i++)
+            int i = 0;
+            for (int frame = 0; frame < 10; frame++)
             {
-                score += rolls[i];
-            }          
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
             return score;
         }
     }
